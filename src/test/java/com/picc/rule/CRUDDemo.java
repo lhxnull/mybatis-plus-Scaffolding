d1380c860ba2bd5222a294d0937f2cb38404f0be
lhxnull <571921459@qq.com>
2019-10-11 15:38:29 +0800

    test测试类

--- src/test/java/com/picc/rule/CRUDDemo.java
+++ src/test/java/com/picc/rule/CRUDDemo.java
@@ -10,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
- * Created by Administrator on 2019/10/9.
+ * Created by Administrator on 2019/10/9.1
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
